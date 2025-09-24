b78e5de8d039233da2324544ba62d61d36000991
AllenGleeson <allen_gleeson@hotmail.com>
2025-09-24 20:43:24 +0100

    Moving header and footer components into layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from '@/components/layout/Header';
-import Footer from '@/components/layout/Footer';
 import Hero from '@/home/components/Hero';
 import About from '@/home/components/About';
 import Portfolio from '@/home/components/Portfolio';
@@ -11,16 +9,12 @@ import Testimonials from '@/home/components/Testimonials';
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Portfolio />
-        <VideoHighlights />
-        <Services />
-        <Testimonials />
-      </main>
-      <Footer />
+      <Hero />
+      <About />
+      <Portfolio />
+      <VideoHighlights />
+      <Services />
+      <Testimonials />
     </div>
   );
 }
