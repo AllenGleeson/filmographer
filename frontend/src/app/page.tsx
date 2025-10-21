174bdf07a3731ff04d5a679456b7c0eef71da8aa
AllenGleeson <allen_gleeson@hotmail.com>
2025-10-21 21:01:49 +0100

    Removed min height class from home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,7 @@ import Testimonials from '@/home/components/Testimonials';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="bg-white">
       <Hero />
       <About />
       <Portfolio />
